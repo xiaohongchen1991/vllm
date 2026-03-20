568dcbfaa24af96a6ad3a05a86a5e7804bab00e6
Sean Chen <seachen@redhat.com>
2026-03-20 17:36:33 -0400

    debug helion gemm autotuning
    Signed-off-by: Sean Chen <seachen@redhat.com>

--- vllm/kernels/helion/ops/scaled_mm.py
+++ vllm/kernels/helion/ops/scaled_mm.py
@@ -36,8 +36,10 @@ def generate_inputs() -> dict[str, tuple[Any, ...]]:
     # TODO(xiaohongchen1991): it is difficult for kernel author to cover
     # all input property combination. Currently, dtypes are fixed. We need
     # optimization to bucket/skip some combinations
-    num_tokens_list = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
-    hidden_size_list = [2048, 4096, 8192]
+    # num_tokens_list = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+    # hidden_size_list = [2048, 4096, 8192]
+    num_tokens_list = [256]
+    hidden_size_list = [2048]
     in_dtype: torch.dtype = current_platform.fp8_dtype()
     scale_dtype: torch.dtype = torch.float32
     out_dtype: torch.dtype = torch.bfloat16
@@ -123,25 +125,25 @@ def scaled_mm(
 ) -> torch.Tensor:
     M, K = a.shape
     N = b.shape[1]
-    hl.specialize(K)
-    hl.specialize(N)
+    # hl.specialize(K)
+    # hl.specialize(N)
 
-    assert N > 0 and K > 0 and M > 0
-    assert b.shape[0] == K
-    assert a.dtype == b.dtype
+    # assert N > 0 and K > 0 and M > 0
+    # assert b.shape[0] == K
+    # assert a.dtype == b.dtype
 
-    scale_a = scale_a.reshape(-1, 1) if scale_a.dim() <= 1 else scale_a
-    scale_b = scale_b.reshape(-1, 1) if scale_b.dim() <= 1 else scale_b
+    # scale_a = scale_a.reshape(-1, 1) if scale_a.dim() <= 1 else scale_a
+    # scale_b = scale_b.reshape(-1, 1) if scale_b.dim() <= 1 else scale_b
 
-    assert scale_a.dtype == scale_b.dtype and scale_a.is_floating_point()
-    assert scale_a.shape[1] == 1 and (scale_a.shape[0] == 1 or scale_a.shape[0] == M)
-    assert scale_b.shape[1] == 1 and (scale_b.shape[0] == 1 or scale_b.shape[0] == N)
-    assert out_dtype.is_floating_point
-    assert _is_weak_contiguous(a)
-    assert _is_weak_contiguous(b)
+    # assert scale_a.dtype == scale_b.dtype and scale_a.is_floating_point()
+    # assert scale_a.shape[1] == 1 and (scale_a.shape[0] == 1 or scale_a.shape[0] == M)
+    # assert scale_b.shape[1] == 1 and (scale_b.shape[0] == 1 or scale_b.shape[0] == N)
+    # assert out_dtype.is_floating_point
+    # assert _is_weak_contiguous(a)
+    # assert _is_weak_contiguous(b)
 
-    if bias is not None:
-        assert bias.numel() == N and bias.dtype == out_dtype
+    # if bias is not None:
+    #     assert bias.numel() == N and bias.dtype == out_dtype
 
     c = torch.empty((M, N), dtype=out_dtype, device=a.device)
     accumulator_dtype = torch.float32 if a.is_floating_point() else torch.int32
@@ -156,18 +158,18 @@ def scaled_mm(
                 out_dtype=accumulator_dtype,
             )
 
-        scale_a_mask = (tile_m.index < scale_a.shape[0])[:, None]
-        scale_a_blk = torch.where(scale_a_mask, scale_a[tile_m, :], scale_a[0, 0])
-        accumulator = scale_a_blk * accumulator.to(torch.float32)
+        # scale_a_mask = (tile_m.index < scale_a.shape[0])[:, None]
+        # scale_a_blk = torch.where(scale_a_mask, scale_a[tile_m, :], scale_a[0, 0])
+        # accumulator = scale_a_blk * accumulator.to(torch.float32)
 
-        scale_b_mask = (tile_n.index < scale_b.shape[0])[:, None]
-        scale_b_blk = torch.where(scale_b_mask, scale_b[tile_n, :], scale_b[0, 0])
-        accumulator = scale_b_blk.T * accumulator.to(torch.float32)
+        # scale_b_mask = (tile_n.index < scale_b.shape[0])[:, None]
+        # scale_b_blk = torch.where(scale_b_mask, scale_b[tile_n, :], scale_b[0, 0])
+        # accumulator = scale_b_blk.T * accumulator.to(torch.float32)
 
         c_blk = accumulator.to(out_dtype)
 
-        if bias is not None:
-            c_blk += bias[tile_n]
+        # if bias is not None:
+        #     c_blk += bias[tile_n]
 
         c[tile_m, tile_n] = c_blk
 
